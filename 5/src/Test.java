ded2f768ffc2e79cc3513aedd49180cc94d9cbdd
Sagat9881 <Sagat9881@gmail.com>
2021-05-29 22:55:18 +0300

    Initial commit

---
+++ 5/src/Test.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class Test {
+    static int b;
+
+    public static void main(String[] args) {
+        Video2.ExampleReadTest();
+        //Video21.ExampleCreate();
+        Video3.Example3();
+        //Чутка изменили
+
+
+    }
+}
